620140531cda0c8d7213d6000055e28a175083c6
Interakcjo <biuro@interakcjo.pl>
2020-04-02 10:32:12 +0200

    Version 2.0 - update checker

--- declaration.php
+++ declaration.php
@@ -4,173 +4,21 @@
 * Description: Wtyczka tworzy stronę z deklaracją dostępności.
 * Author: Przemysław Drożniak & Ernest Fichtner
 * Text Domain: declaration
-* Version: 1.4
+* Version: 2.0
 */
 
 if(defined('WP_DEBUG') && WP_DEBUG) {
     require_once __DIR__ . DIRECTORY_SEPARATOR . 'debugger.php';
 }
 
-define( 'DECLARATION_VERSION', '1.4' );
+// Update Checker
+require dirname(__FILE__) . '/plugin-update-checker-master/plugin-update-checker.php';
 
-add_filter('plugins_api', 'declaration_plugin_info', 20, 3);
-/*
- * $res empty at this step
- * $action 'plugin_information'
-*/
-function declaration_plugin_info( $res, $action, $args ) {
-    // do nothing if this is not about getting plugin information
-    if( 'plugin_information' !== $action ) {
-        return false;
-    }
-
-    $plugin_slug = 'declaration'; // we are going to use it in many places in this function
-
-    // do nothing if it is not our plugin
-    if( $plugin_slug !== $args->slug ) {
-        return false;
-    }
-
-    // trying to get from cache first
-    if( false == $remote = get_transient( 'declaration_update_' . $plugin_slug ) ) {
-        // info.json is the file with the actual plugin information on your server
-        $remote = wp_remote_get( 'http://wptest.nowoczesnyurzad.pl/info.json', array(
-            'timeout' => 10,
-            'headers' => array(
-                'Accept' => 'application/json'
-            ) )
-        );
-
-        if ( ! is_wp_error( $remote ) && isset( $remote['response']['code'] ) && $remote['response']['code'] == 200 && ! empty( $remote['body'] ) ) {
-            set_transient( 'declaration_update_' . $plugin_slug, $remote, 60 ); // 12 hours cache
-        }
-    }
-
-    if( ! is_wp_error( $remote ) && isset( $remote['response']['code'] ) && $remote['response']['code'] == 200 && ! empty( $remote['body'] ) ) {
-        $remote = json_decode( $remote['body'] );
-
-        $res = new stdClass();
-
-        $res->name = 'Declaration';
-        $res->slug = $plugin_slug;
-        $res->version = $remote->version;
-        $res->tested = $remote->tested;
-        $res->requires = $remote->requires;
-        $res->author = 'Przemysław Drożniak & Ernest Fichtner';
-        $res->download_link = $remote->download_url;
-        $res->trunk = $remote->download_url;
-        $res->requires_php = $remote->requires_php;
-        $res->last_updated = $remote->last_updated;
-        $res->sections = array(
-            'description' => $remote->sections->description,
-            'installation' => $remote->sections->installation
-        );
-
-        $res->banners = array(
-            'low' => 'https://source.unsplash.com/random/772x250',
-            'high' => 'https://source.unsplash.com/random/1544x500'
-        );
-
-        return $res;
-    }
-
-    return false;
-}
-
-add_filter('site_transient_update_plugins', 'declaration_push_update' );
-
-function declaration_push_update( $transient ) {
-    if ( empty($transient->checked ) ) {
-        return $transient;
-    }
-
-    // trying to get from cache first, to disable cache comment 10,20,21,22,24
-    if( false == $remote = get_transient( 'declaration_upgrade_declaration' ) ) {
-        // info.json is the file with the actual plugin information on your server
-        $remote = wp_remote_get( 'http://wptest.nowoczesnyurzad.pl/info.json', array(
-            'timeout' => 10,
-            'headers' => array(
-                'Accept' => 'application/json'
-            ) )
-        );
-
-        if ( !is_wp_error( $remote ) && isset( $remote['response']['code'] ) && $remote['response']['code'] == 200 && !empty( $remote['body'] ) ) {
-            set_transient( 'declaration_upgrade_declaration', $remote, 60 ); // 12 hours cache
-        }
-    }
-
-    if( $remote ) {
-      $status = false;
-      if(!is_wp_error($remote)){
-        $response = json_decode( $remote['body'] );
-        if(isset($response->status)){
-          $status = $response->status;
-        }
-      }
-       return $status;
-        // your installed plugin version should be on the line below! You can obtain it dynamically of course
-        if( $remote && version_compare(DECLARATION_VERSION, $remote->version, '<') ) {
-            $res = new stdClass();
-            $res->slug = 'declaration';
-            $res->plugin = 'declaration/declaration.php'; // it could be just YOUR_PLUGIN_SLUG.php if your plugin doesn't have its own directory
-            $res->new_version = $remote->version;
-            $res->tested = $remote->tested;
-            $res->package = $remote->download_url;
-                $transient->response[$res->plugin] = $res;
-                //$transient->checked[$res->plugin] = $remote->version;
-        }
-    }
-
-    return $transient;
-}
-
-// add_action( 'upgrader_process_complete', 'declaration_after_update', 10, 2 );
-
-// function declaration_after_update( $upgrader_object, $options ) {
-//  if ( $options['action'] == 'update' && $options['type'] == 'plugin' )  {
-//         // just clean the cache when new plugin version is installed
-//         delete_transient( 'declaration_upgrade_declaration' );
-//  }
-// }
-
-// function bgmc_plugins_update_completed( $upgrader_object, $options ) {
-
-//     // If an update has taken place and the updated type is plugins and the plugins element exists
-//     if ( $options['action'] == 'update' && $options['type'] == 'plugin' && isset( $options['plugins'] ) ) {
-//         // code here
-//     }
-// }
-// add_action( 'upgrader_process_complete', 'bgmc_plugins_update_completed', 10, 2 );
-
-$declaration_version = '1.4';
-
-add_action('upgrader_process_complete', 'declaration_upgrade', 10,2);
-function declaration_upgrade(\WP_Upgrader $upgrader_object, $hook_extra){
-    global $declaration_version;
-
-    if(is_array($hook_extra) && array_key_exists('action', $hook_extra) && array_key_exists('type', $hook_extra) && array_key_exists('plugins', $hook_extra)) {
-        if($hook_extra['action'] == 'update' && $hook_extra['type'] == 'plugin' && is_array($hook_extra['plugins']) && !empty($hook_extra['plugins'])) {
-            $this_plugin = plugin_basename(__FILE__);
-            foreach ($hook_extra['plugins'] as $each_pluginn) {
-                if($each_plugin == $this_plugin){
-                    file_put_contents(dirname(__FILE__) . '/test.txt', 'v' . $declaration_version."\r\n", FILE_APPEND);
-                    set_transient('declaration_updated', 1);
-                }
-            }
-            unset($each_plugin);
-        }
-    }
-}
-
-add_action('plugin_loaded', 'declaration_runUpdatedPlugin');
-function declaration_runUpdatedPlugin(){
-    global $declaration_version;
-    if(get_transient('declaration_updated') && current_user_can('menage_options')){
-        file_put_contents(dirname(__FILE__). '/test-update-by-transient.txt', 'v' . $declaration_version."\r\n", FILE_APPEND);        
-
-        delete_transient('declaration_updated');        
-    }
-}
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'http://wptest.nowoczesnyurzad.pl/plugin.json',
+	__FILE__, //Full path to the main plugin file or functions.php.
+	'declaration'
+);
 
 // Create theme templates
 if(!function_exists('declaration_theme_files')) {
@@ -441,7 +289,3 @@ if(! function_exists('save_details')) {
     }
     add_action('save_post', 'save_details', 100);
 }
-
-// Delete transient
-delete_transient( 'declaration_upgrade_declaration' );
-wp_cache_flush();
