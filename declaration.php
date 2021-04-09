17befb0e89d5280874fe157b17824ecec2370d2b
Fichtner21 <ernest.fichtner@gmail.com>
2021-04-09 13:06:16 +0200

    Update -> update checker

--- declaration.php
+++ declaration.php
@@ -8,7 +8,7 @@ Plugin Name: Declaration
 Description: Wtyczka tworzy stronę z deklaracją dostępności.
 Author: Przemysław Drożniak & Ernest Fichtner
 Text Domain: declaration
-Version: 1.2.4
+Version: 1.2.5
 */
 
 defined( 'ABSPATH' ) or die('Sorry, you cant access to this site!');
